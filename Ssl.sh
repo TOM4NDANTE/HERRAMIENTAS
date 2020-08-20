cb63fa70f84aa7672c0b46516d354948f9de7671
TOM4NDANTE <56950552+TOM4NDANTE@users.noreply.github.com>
2020-08-19 23:53:41 -0500

    Update Ssl.sh

--- Ssl.sh
+++ Ssl.sh
@@ -1,187 +1,163 @@
-#!/bin/bash
-#19/12/2019
-declare -A cor=( [0]="\033[1;37m" [1]="\033[1;34m" [2]="\033[1;31m" [3]="\033[1;33m" [4]="\033[1;32m" )
-SCPfrm="/etc/ger-frm" && [[ ! -d ${SCPfrm} ]] && exit
-SCPinst="/etc/ger-inst" && [[ ! -d ${SCPinst} ]] && exit
-mportas () {
-unset portas
-portas_var=$(lsof -V -i tcp -P -n | grep -v "ESTABLISHED" |grep -v "COMMAND" | grep "LISTEN")
-while read port; do
-var1=$(echo $port | awk '{print $1}') && var2=$(echo $port | awk '{print $9}' | awk -F ":" '{print $2}')
-[[ "$(echo -e $portas|grep "$var1 $var2")" ]] || portas+="$var1 $var2\n"
-done <<< "$portas_var"
-i=1
-echo -e "$portas"
-}
-fun_ip () {
-MEU_IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
-MEU_IP2=$(wget -qO- ipv4.icanhazip.com)
-[[ "$MEU_IP" != "$MEU_IP2" ]] && IP="$MEU_IP2" || IP="$MEU_IP"
-}
-fun_eth () {
-eth=$(ifconfig | grep -v inet6 | grep -v lo | grep -v 127.0.0.1 | grep "encap:Ethernet" | awk '{print $1}')
-    [[ $eth != "" ]] && {
-    msg -bar
-    echo -e "${cor[3]} $(fun_trans  "Aplicar el sistema para mejorar los paquetes SSH?")"
-    echo -e "${cor[3]} $(fun_trans  "Opciones para usuarios avanzados")"
-    msg -bar
-    read -p " [S/N]: " -e -i n sshsn
-           [[ "$sshsn" = @(s|S|y|Y) ]] && {
-           echo -e "${cor[1]} $(fun_trans  "Corrección de problemas de paquetes en SSH...")"
-           echo -e " $(fun_trans  "¿Cual es la tasa RX?")"
-           echo -ne "[ 1 - 999999999 ]: "; read rx
-           [[ "$rx" = "" ]] && rx="999999999"
-           echo -e " $(fun_trans  "¿Cuál es la tarifa TX?")"
-           echo -ne "[ 1 - 999999999 ]: "; read tx
-           [[ "$tx" = "" ]] && tx="999999999"
-           apt-get install ethtool -y > /dev/null 2>&1
-           ethtool -G $eth rx $rx tx $tx > /dev/null 2>&1
-           }
-     msg -bar
-     }
-}
-fun_bar () {
-comando="$1"
- _=$(
-$comando > /dev/null 2>&1
-) & > /dev/null
-pid=$!
-while [[ -d /proc/$pid ]]; do
-echo -ne " \033[1;33m["
-   for((i=0; i<10; i++)); do
-   echo -ne "\033[1;31m##"
-   sleep 0.2
-   done
-echo -ne "\033[1;33m]"
-sleep 1s
-echo
-tput cuu1
-tput dl1
-done
-echo -e " \033[1;33m[\033[1;31m####################\033[1;33m] - \033[1;32m100%\033[0m"
-sleep 1s
-}
-ssl_stunel () {
-[[ $(mportas|grep stunnel4|head -1) ]] && {
-echo -e "\033[1;33m $(fun_trans  "Parando Stunnel")"
-msg -bar
-fun_bar "apt-get purge stunnel4 -y"
-msg -bar
-echo -e "\033[1;33m $(fun_trans  "Parado Con Exito!")"
-msg -bar
-return 0
-}
-echo -e "\033[1;32m $(fun_trans  "INSTALADOR SSL By MOD MX")"
-msg -bar
-echo -e "\033[1;33m $(fun_trans  "Seleccione una puerta de redirección interna.")"
-echo -e "\033[1;33m $(fun_trans  "Es decir, un puerto en su servidor para SSL")"
-msg -bar
-         while true; do
-         echo -ne "\033[1;37m"
-         read -p " Local-Port: " portx
-         if [[ ! -z $portx ]]; then
-             if [[ $(echo $portx|grep [0-9]) ]]; then
-                [[ $(mportas|grep $portx|head -1) ]] && break || echo -e "\033[1;31m $(fun_trans  "Puerta invalida")"
-             fi
-         fi
-         done
-msg -bar
-DPORT="$(mportas|grep $portx|awk '{print $2}'|head -1)"
-echo -e "\033[1;33m $(fun_trans  "Ahora Prestamos Saber Que Puerta del SSL, Va a Escuchar")"
-msg -bar
-    while true; do
-    read -p " Listen-SSL: " SSLPORT
-    [[ $(mportas|grep -w "$SSLPORT") ]] || break
-    echo -e "\033[1;33m $(fun_trans  "Esta puerta está en uso")"
-    unset SSLPORT
-    done
-msg -bar
-echo -e "\033[1;33m $(fun_trans  "Instalando SSL")"
-msg -bar
-fun_bar "apt-get install stunnel4 -y"
-echo -e "client = no\n[SSL]\ncert = /etc/stunnel/stunnel.pem\naccept = ${SSLPORT}\nconnect = 127.0.0.1:${DPORT}" > /etc/stunnel/stunnel.conf
-####Coreccion2.0##### 
-openssl genrsa -out stunnel.key 2048 > /dev/null 2>&1
+#/bin/bash
 
-(echo "mx" ; echo "mx" ; echo "mx" ; echo "mx" ; echo "mx" ; echo "mx" ; echo "@vpsmx" )|openssl req -new -key stunnel.key -x509 -days 1000 -out stunnel.crt > /dev/null 2>&1
+echo -e "\033[1;33m- - - - -> \033[01;34mScript para configurar certificado SSL"
+echo -e "\033[1;33m #################"
+echo -e "\033[1;31mTelegram @XTOM4NDANTE"
+sleep 2
 
+apt-get update -y
+clear
+yum update -y
+apt-get install openssh-server -y
+clear
+apt-get install curl -y
+clear
+yum install openssh-server -y
+clear
+apt-get install openssh-client -y
+clear
+yum install openssh-client -y
+clear
+apt-get install stunnel4 -y
+clear
+yum install stunnel4 -y
+clear
+apt-get install stunnel -y
+clear
+yum install stunnel -y
+clear
+
+echo -e "\033[1;31mCAPTURANDO IP"
+ip=$(curl https://api.ipify.org/)
+echo $ip
+clear
+
+echo -e "\033[1;33m ######################################"
+echo -e "\033[1;31mPRESIONA ENTER"
+echo -e "\033[1;33m #################"
+sleep 1
+
+echo -e "\033[1;33m ######################################"
+echo -e "\033[1;31mGENERANDO CERTIFICADO"
+echo -e "\033[1;33m #################"
+sleep 1
+openssl genrsa 2048 > stunnel.key
+openssl req -new -key stunnel.key -x509 -days 1000 -out stunnel.crt
+
+echo -e "\033[1;31mCREANDO NUEVA CONFIGURACIÓN"
+echo -e "\033[1;33m ######################################"
+sleep 2
+rm /etc/stunnel/stunnel.conf
+clear
+rm /etc/default/stunnel4
+clear
 cat stunnel.crt stunnel.key > stunnel.pem 
-
 mv stunnel.pem /etc/stunnel/
-######-------
-sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
-service stunnel4 restart > /dev/null 2>&1
-msg -bar
-echo -e "\033[1;33m $(fun_trans  "INSTALADO CON EXITO")"
-msg -bar
-rm -rf /etc/ger-frm/stunnel.crt > /dev/null 2>&1
-rm -rf /etc/ger-frm/stunnel.key > /dev/null 2>&1
-rm -rf /root/stunnel.crt > /dev/null 2>&1
-rm -rf /root/stunnel.key > /dev/null 2>&1
-return 0
-}
-ssl_stunel_2 () {
-echo -e "\033[1;32m $(fun_trans  "INSTALADOR SSL By @Kalix1")"
-msg -bar
-echo -e "\033[1;33m $(fun_trans  "Seleccione una puerta de redirección interna.")"
-echo -e "\033[1;33m $(fun_trans  "Es decir, un puerto en su servidor para SSL")"
-msg -bar
-         while true; do
-         echo -ne "\033[1;37m"
-         read -p " Local-Port: " portx
-         if [[ ! -z $portx ]]; then
-             if [[ $(echo $portx|grep [0-9]) ]]; then
-                [[ $(mportas|grep $portx|head -1) ]] && break || echo -e "\033[1;31m $(fun_trans  "Puerta invalida")"
-             fi
-         fi
-         done
-msg -bar
-DPORT="$(mportas|grep $portx|awk '{print $2}'|head -1)"
-echo -e "\033[1;33m $(fun_trans  "Ahora Prestamos Saber Que Puerta del SSL, Va a Escuchar")"
-msg -bar
-    while true; do
-    read -p " Listen-SSL: " SSLPORT
-    [[ $(mportas|grep -w "$SSLPORT") ]] || break
-    echo -e "\033[1;33m $(fun_trans  "Esta puerta está en uso")"
-    unset SSLPORT
-    done
-msg -bar
-echo -e "\033[1;33m $(fun_trans  "Instalando SSL")"
-msg -bar
-fun_bar "apt-get install stunnel4 -y"
-echo -e "client = no\n[SSL+]\ncert = /etc/stunnel/stunnel.pem\naccept = ${SSLPORT}\nconnect = 127.0.0.1:${DPORT}" >> /etc/stunnel/stunnel.conf
-######-------
-sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
-service stunnel4 restart > /dev/null 2>&1
-msg -bar
-echo -e "\033[1;33m $(fun_trans  "INSTALADO CON EXITO")"
-msg -bar
-rm -rf /etc/ger-frm/stunnel.crt > /dev/null 2>&1
-rm -rf /etc/ger-frm/stunnel.key > /dev/null 2>&1
-rm -rf /root/stunnel.crt > /dev/null 2>&1
-rm -rf /root/stunnel.key > /dev/null 2>&1
-return 0
-}
-echo -e "${cor[3]}INSTALADOR SSL By @Kalix1"
-msg -bar
-echo -e "${cor[1]} Escoja la opcion deseada."
-msg -bar
-echo "1).- ININICIAR O PARAR SSL"
-echo "2).- AGREGAR PUERTOS SSL"
-msg -bar
-echo -n "Digite solo el numero segun su respuesta: "
-read opcao
-case $opcao in
-1)
-msg -bar
-ssl_stunel
-;;
-2)
-msg -bar
-echo -e "\033[1;93m  AGREGAR SSL EXTRA  ..."
-msg -bar
-ssl_stunel_2
-sleep 3
-exit
-;;
-esac
+clear
+echo -e "\033[1;31mESCRIBE EL PUERTO SSL DE ESCUCHA(442sugerido)"
+echo -e "\033[1;31mPUERTO   S S L DIRECTO"
+read -p ": " portssl
+clear
+echo -e "\033[1;31mESCRIBE EL PUERTO LOCAL SHADOWSOCKS PYTHON(445sugerido)"
+echo -e "\033[1;31mPUERTO   LOCAL"
+read -p ": " portlocal
+clear
+echo -e "\033[1;31mESCRIBE EL PUERTO DE ESCUCHA SSL SHADOWSOCKS PYTHON(521sugerido)"
+echo -e "\033[1;31mPUERTO   SSL ESCUCHA"
+read -p ": " portshadow
+clear
+
+echo -e "\033[1;33m ######################################"
+echo -e "\033[1;31mC O N F I G U R A N D O  STUNNEL.CONF"
+echo -e "\033[1;33m #################"
+sleep 1
+
+echo -e "\033[1;31m ###"
+sleep 1
+echo -e "\033[1;31m #########"
+sleep 1
+echo -e "\033[1;31m ################"
+sleep 1
+echo -e "\033[1;31m ########################"
+sleep 1
+echo -e "\033[1;31m ##################################"
+sleep 1
+
+
+echo "cert = /etc/stunnel/stunnel.pem " >> /etc/stunnel/stunnel.conf
+echo "client = no " >> /etc/stunnel/stunnel.conf
+echo "sslVersion = ALL " >> /etc/stunnel/stunnel.conf
+echo "socket = a:SO_REUSEADDR=1 " >> /etc/stunnel/stunnel.conf
+echo "socket = l:TCP_NODELAY=1 " >> /etc/stunnel/stunnel.conf
+echo "socket = r:TCP_NODELAY=1 " >> /etc/stunnel/stunnel.conf
+echo "" >> /etc/stunnel/stunnel.conf
+echo "[ssh] " >> /etc/stunnel/stunnel.conf
+echo "connect = 127.0.0.1:22 " >> /etc/stunnel/stunnel.conf
+echo "accept = $portssl" >> /etc/stunnel/stunnel.conf
+echo -e "\033[1;31mAGREGANDO PUERTO LOCAL PARA SHADOWSOCKS:\033[0m $portlocal" 
+echo -e "\033[1;31mPUERTO DE ESCUCHA SHADOWSOCKS SSL:\033[0m $portshadow"
+echo "" >> /etc/stunnel/stunnel.conf
+echo "[stunnel] " >> /etc/stunnel/stunnel.conf
+echo "connect = 127.0.0.1:$portlocal " >> /etc/stunnel/stunnel.conf
+echo "accept = $portshadow " >> /etc/stunnel/stunnel.conf
+
+
+echo -e "\033[1;33m ######################################"
+echo -e "\033[1;31mC O N F I G U R A N D O  STUNNEL4"
+echo -e "\033[1;33m ###################"
+sleep 1
+
+echo "ENABLED=1 " >> /etc/default/stunnel4
+echo "FILES="/etc/stunnel/*.conf" " >> /etc/default/stunnel4
+echo "OPTIONS="" " >> /etc/default/stunnel4
+echo "PPP_RESTART=0" >> /etc/default/stunnel4
+
+echo -e "\033[1;33m ######################################"
+echo -e "\033[1;31mI N I C I A N D O  STUNNEL4"
+echo -e "\033[1;33m ###################"
+sleep 1
+
+echo -e "\033[1;33m ######################################"
+echo -e "\033[1;31m ##### PUEDEN SALIR ALGUNOS ERRORES NO TE PREOCUPES...######"
+echo -e "\033[1;33m ######################################"
+sleep 1
+service ssh start 1>/dev/null 2 /dev/null
+/etc/init.d/ssh start 1>/dev/null 2 /dev/null
+service sshd start 1>/dev/null 2 /dev/null
+/etc/init.d/sshd start 1>/dev/null 2 /dev/null
+service sshd restart 1>/dev/null 2 /dev/null
+/etc/init.d/sshd restart 1>/dev/null 2 /dev/null
+service ssh restart 1>/dev/null 2 /dev/null
+/etc/init.d/ssh restart 1>/dev/null 2 /dev/null
+service stunnel4 start 1>/dev/null 2 /dev/null
+/etc/init.d/stunnel4 start 1>/dev/null 2 /dev/null
+service stunnel4 restart
+/etc/init.d/stunnel4 restart 1>/dev/null 2 /dev/null
+systemctl start stunnel4 1>/dev/null 2 /dev/null
+systemctl restart stunnel 1>/dev/null 2 /dev/null
+clear
+
+echo -e "\033[1;33m #######ALGUNOS ERRORES SALDRÁN###############"
+sleep 2
+echo -e "\033[1;33m ###########REINICIADO...###########"
+clear
+
+echo -e "\033[1;33m ###################"
+echo -e "\033[1;31mCOMPLETADA LA INSTALACIÓN"
+echo -e "\033[1;33m ######################################"
+echo -e "\033[1;33m- - - - -> \033[01;34mSU IP DEL HOST:\033[0m $ip"
+echo -e "\033[1;33m- - - - -> \033[01;34mPUERTO SSL DIRECTO:\033[0m $portssl"
+sleep 1
+echo -e "\033[1;33m- - - - -> \033[01;34mPUERTO SHADOWSOCKS PYTHON LOCAL:\033[0m $portlocal"
+sleep 1
+echo -e "\033[1;33m- - - - -> \033[01;34mPUERTO SHADOWSOCKS PYTHON SSL:\033[0m $portshadow"
+sleep 1
+echo -e "\033[1;31mES NECESARIO REINICIAR"
+sleep 2
+echo -e "\033[1;33m- - ->>ⅅℐЅᏆℛℐℬUℐⅅᎾℛ  ℕℰᏆℱℛℰℰ  \033[01;34m"
+sleep 1
+echo -e "\033[1;33m- - ->>TELEGRAM \033[01;34m+529331183529"
+sleep 1
+echo -e "\033[1;33m- - ->>║▌XᏆᎾℳ4ℕⅅᎯℕᏆℰX ® ▌║█ \033[01;34m "
+sleep 1
+rm ssltls*
